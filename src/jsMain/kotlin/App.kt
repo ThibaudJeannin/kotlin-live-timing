e477334a764864db006c8b8d309fbe91be99c4df
Thibaud Jeannin <thibaud.jeannin@gmail.com>
2021-11-20 02:12:52 +0100

    Amélioré style Passé à Kotlin 1.6

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,8 +1,17 @@
+import csstype.Border
+import csstype.FontFamily
 import io.live.timing.TimeBoard
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import react.*
+import kotlinx.css.*
+import kotlinx.css.properties.LineHeight
+import kotlinx.css.properties.border
+import react.PropsWithChildren
 import react.dom.*
+import react.fc
+import react.useEffectOnce
+import react.useState
+import styled.*
 
 private val scope = MainScope()
 
@@ -27,42 +36,77 @@ val app = fc<PropsWithChildren> {
         }
     }
 
-    table {
-        thead {
-            tr {
-                th {
-                    +"number"
-                }
-                th {
-                    +"pilot"
-                }
-                th {
-                    +"laptime"
+    styledDiv {
+        css {
+            borderStyle = BorderStyle.solid
+            borderColor = Color.red
+            borderWidth = 5.px
+            position = Position.relative
+            top = 10.px
+            left = 10.px
+        }
+        styledTable {
+            css {
+                borderCollapse = BorderCollapse.collapse
+            }
+            styledThead {
+                tr {
+                    styledTh {
+                        +"number"
+                    }
+                    th {
+                        +"pilot"
+                    }
+                    th {
+                        +"laptime"
+                    }
                 }
             }
-        }
-        tbody {
-            for ((pilot, lapTime) in timeBoard.sortedResults()) {
+            styledTbody {
+                for ((pilot, lapTime) in timeBoard.sortedResults()) {
 
-                tr {
-                    td {
-                        +"${pilot.number}"
-                    }
-                    td {
-                        +pilot.name
-                    }
-                    td {
-                        if (lapTime != null) {
-                            +("${lapTime.minutes}" +
-                                    ":${lapTime.seconds.toString().padStart(2, '0')}" +
-                                    ".${lapTime.millis.toString().padStart(3, '0')}")
-                        } else {
-                            +"no time"
+                    styledTr {
+                        css {
+                            backgroundColor = Color.slateGrey
+                            borderRadius = 10.px
+                            color = Color.whiteSmoke
+                            height = 40.px
+                        }
+                        styledTd {
+                            styledDiv {
+                                css {
+                                    val size = 25.px
+
+                                    textAlign = TextAlign.center
+                                    backgroundColor = Color.darkSlateGrey
+                                    borderRadius = 10.px
+                                    width = size
+                                    height = size
+                                    lineHeight = LineHeight("$size")
+                                    fontWeight = FontWeight("900")
+                                    color = Color.beige
+                                }
+                                +"${pilot.number}"
+                            }
+                        }
+                        styledTd {
+                            css {
+                                fontWeight = FontWeight("700")
+                            }
+                            +pilot.name
+                        }
+                        td {
+                            if (lapTime != null) {
+                                +("${lapTime.minutes}" +
+                                        ":${lapTime.seconds.toString().padStart(2, '0')}" +
+                                        ".${lapTime.millis.toString().padStart(3, '0')}")
+                            } else {
+                                +"no time"
+                            }
                         }
                     }
                 }
             }
         }
-
     }
 }
