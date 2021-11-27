89eae5fc2a9308e4df93b36b34f643542e892b3e
thibaudjeannin <thibaud.jeannin@gmail.com>
2021-11-27 23:54:25 +0100

    Test Tomcat

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -11,7 +11,7 @@ import io.live.timing.TimeBoard
 import java.io.File
 import kotlin.random.Random
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
     routing {
