a44c8d6a5046c0d80c85b0e1a4603f575e3eaa87
Thibaud Jeannin <thibaud.jeannin@gmail.com>
2021-11-30 20:58:44 +0100

    tomcat server engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,8 @@ kotlin {
             dependencies {
                 implementation("io.ktor:ktor-serialization:1.6.6")
                 implementation("io.ktor:ktor-server-core:1.6.6")
-                implementation("io.ktor:ktor-server-cio:1.6.6")
+//                implementation("io.ktor:ktor-server-cio:1.6.6")
+                implementation("io.ktor:ktor-server-tomcat:1.6.6")
 //                implementation("io.ktor:ktor-server-netty:1.6.6")
 //                implementation("io.ktor:ktor-server-jetty:1.6.6")
                 implementation("ch.qos.logback:logback-classic:1.2.7")
